b54c7180163bbdaed132aa16a46b8a6056fe1f31
Fuad_149 <fuadhossan.tlc22@gmail.com>
2025-09-20 23:34:34 +0600

    Update numerical_differentiation_code.cpp

--- code/numerical_differentiation_code.cpp
+++ code/numerical_differentiation_code.cpp
@@ -1,1 +1,27 @@
+// Muhammad fuad 2024000000149
 
+#include <bits/stdc++.h>
+using namespace std;
+
+//Approximates the derivative of a function using the Forward Difference Formula.
+double forward(double (*f)(double), double x, double h)
+{
+    return (f(x + h) - f(x)) / h;
+}
+
+//Approximates the derivative of a function using the Backward Difference Formula.
+double backward(double (*f)(double), double x, double h)
+{
+    return (f(x) - f(x - h)) / h;
+}
+
+//Approximates the derivative of a function using the Central Difference Formula.
+double central(double (*f)(double), double x, double h)
+{
+    return (f(x + h) - f(x - h)) / (2 * h);
+}
+
+//These are not in <cmath>, so we define them manually.
+double sec(double x)      { return 1.0 / cos(x); }
+double cosec(double x)    { return 1.0 / sin(x); }
+double cot(double x)      { return cos(x) / sin(x); }
